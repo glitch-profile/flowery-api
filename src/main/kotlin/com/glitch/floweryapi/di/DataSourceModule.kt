dd03a5b247e2681a232b85feeb85608b262aa992
Ivan <korablev2002@yandex.ru>
2024-07-30 21:38:53 +0400

    added dependency injection for data sources

--- src/main/kotlin/com/glitch/floweryapi/di/DataSourceModule.kt
+++ src/main/kotlin/com/glitch/floweryapi/di/DataSourceModule.kt
@@ -1,11 +1,16 @@
 package com.glitch.floweryapi.di
 
+import com.glitch.floweryapi.data.datasource.ClientsDataSource
 import com.glitch.floweryapi.data.datasource.PersonsDataSource
+import com.glitch.floweryapi.data.datasourceimpl.users.ClientsDataSourceImpl
 import com.glitch.floweryapi.data.datasourceimpl.users.PersonsDataSourceImpl
 import org.koin.dsl.module
 
 val dataSourceModule = module {
     single<PersonsDataSource> {
         PersonsDataSourceImpl(db = get())
+    }
+    single<ClientsDataSource> {
+        ClientsDataSourceImpl(db = get())
     }
 }
--- src/main/kotlin/com/glitch/floweryapi/plugins/Routing.kt
+++ src/main/kotlin/com/glitch/floweryapi/plugins/Routing.kt
@@ -1,12 +1,20 @@
 package com.glitch.floweryapi.plugins
 
+import com.glitch.floweryapi.data.datasource.ClientsDataSource
+import com.glitch.floweryapi.data.datasource.PersonsDataSource
 import com.glitch.floweryapi.routes.authorizationRoutes
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Application.configureRouting() {
 
     routing {
+
+        //users datasource
+        val personsDataSource by inject<PersonsDataSource>()
+        val clientsDataSource by inject<ClientsDataSource>()
+
         authorizationRoutes()
     }
 
